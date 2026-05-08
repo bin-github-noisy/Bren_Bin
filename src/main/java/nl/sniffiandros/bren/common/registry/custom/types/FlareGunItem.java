57dc32ca1c0c80286ce08f188f5e8c2c39fe8345
binbin2007 <3471903502@qq.com>
2026-05-08 13:18:56 +0800

    Fixed the ammo display HUD

--- src/main/java/nl/sniffiandros/bren/common/registry/custom/types/FlareGunItem.java
+++ src/main/java/nl/sniffiandros/bren/common/registry/custom/types/FlareGunItem.java
@@ -4,7 +4,7 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import nl.sniffiandros.bren.common.registry.custom.PoseType;
 
-public class FlareGunItem extends ShotgunItem  {
+public class FlareGunItem extends GunWithMagItem  {
     public FlareGunItem(Item.Properties settings) {
         super(settings);
     }
@@ -17,7 +17,7 @@ public class FlareGunItem extends ShotgunItem  {
     public int getMaxCapacity(ItemStack stack) {
         // 为霰弹枪设置合适的容量，通常霰弹枪有6-8发容量
         // 这里设置为8发，可以根据实际需求调整
-        return 1; // 修复：直接返回int值，而不是Optional.of(8)
+        return 6; // 修复：直接返回int值，而不是Optional.of(8)
     }
 
     @Override
